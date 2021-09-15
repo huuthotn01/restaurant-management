f05f576566ac9fc6042ad5a5a487f2f6e76bae5b
huuthotn01 <tho.trannguyenhuu@hcmut.edu.vn>
2021-09-15 23:37:50 +0700

    Router

---
+++ client/src/index.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Container} from 'reactstrap';
+import {Switch, Route, BrowserRouter, Redirect} from 'react-router-dom';
+
+class FromHome extends React.Component {
+    render() {
+        return (
+            <h1>Redirect from Home ('/')</h1>
+        );
+    }
+}
+
+class FromTest extends React.Component {
+    render() {
+        return (
+            <h1>Redirect from Test ('/test')</h1>
+        );
+    }
+}
+
+class First extends React.Component {
+    render() {
+        return (
+            <BrowserRouter>
+                <Switch>
+                    <Redirect exact from="/" to="/test1" />
+                    <Redirect exact from="/test" to="/test2" />
+                    <Route path="/test1">
+                        <FromHome />
+                    </Route>
+                    <Route path="/test2">
+                        <FromTest />
+                    </Route>
+                </Switch>
+            </BrowserRouter>
+        );
+    }
+}
+
+ReactDOM.render(
+    <First />,
+    document.getElementById('root')
+);
