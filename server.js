a5bd3b9496b19bb16a18e87f960d6247165df902
huuthotn01 <tho.trannguyenhuu@hcmut.edu.vn>
2021-09-15 23:43:40 +0700

    Router

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ app.listen(port, () => console.log(`Listening on port ${port}`));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'client/build')));
 
+/*
 app.get('/', function(req, res) {
     res.redirect(301, '/home');
     console.log("Root");
@@ -20,3 +21,4 @@ app.get('/home', function(req, res) {
 app.use(function(req, res, next) {
     res.status(404).send('<h1>Nothing found</h1>');
 });
+*/
