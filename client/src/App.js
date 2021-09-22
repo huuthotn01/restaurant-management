63a15704241fea9e8d5e60be8b591706aa71ade7
huuthotn01 <tho.trannguyenhuu@hcmut.edu.vn>
2021-09-22 16:46:49 +0700

    Test

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import LoginWindow from './components/LoginWindow';
+import LoginWindow from './login/LoginWindow';
 import {Button} from 'react-bootstrap';
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
