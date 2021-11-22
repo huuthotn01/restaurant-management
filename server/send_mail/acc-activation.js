eee05c206878902bd30b31f3b23c35ff1c33b2e2
Gia Cat <giacatnk@gmail.com>
2021-11-22 15:43:35 +0700

    Merge

--- server/send_mail/acc-activation.js
+++ server/send_mail/acc-activation.js
@@ -2,31 +2,37 @@ var nodemailer = require('nodemailer');
 var fs = require('fs');
 var util = require('util');
 
-var html_content = fs.readFileSync('./index.html');
-html_content = html_content.toString();
-var output = util.format(html_content, 'GCAbyss', '', 'yellow-bee.herokuapp.com', 'yellow-bee.herokuapp.com');
+async function accActivation(protocol, host, username, email, act_id) {
+    let html_content = fs.readFileSync('./send_mail/acc-activation.html');
+    html_content = html_content.toString();
+    let activation_link = protocol + "://" + host + "/activate/" + act_id;
+    console.log(activation_link);
+    let output = util.format(html_content, username, '', activation_link, activation_link);
 
-var transporter = nodemailer.createTransport({
-    service: 'Gmail',
-    auth: {
-        user: 'yellowbee.pos@gmail.com',
-        pass: 'yellowbeeJaCac'
+    let transporter = nodemailer.createTransport({
+        service: 'Gmail',
+        auth: {
+            user: 'yellowbee.pos@gmail.com',
+            pass: 'yellowbeeJaCac'
+        }
+    });
+
+    let mainOptions = {
+        from: "Aprycot",
+        to: email,
+        subject: 'Aprycot: Kích hoạt tài khoản của bạn',
+        text: 'Kích hoạt tài khoản',
+        html: output
     }
-});
 
-var mainOptions = {
-    from: "Yellow Bee",
-    to: 'cat01697351001@gmail.com',
-    subject: '[YELLOW BEE] KÍCH HOẠT TÀI KHOẢN',
-    text: 'Message from YELLOW BEE',
-    html: output
+    transporter.sendMail(mainOptions, (err, info) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("Info: ", info);
+            console.log("Message sent.");
+        }
+    });
 }
 
-transporter.sendMail(mainOptions, (err, info) => {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log("Infp: ", info);
-        console.log("Message sent.");
-    }
-});
+module.exports = accActivation;
