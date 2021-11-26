4d0443e18df8bd647f690fb5356209c5145fbfc7
Ha Bac Ngo <ngothihabac2772001@gmail.com>
2021-11-26 17:14:16 +0700

    Update English

--- client/src/features/TableReservation/ReservationPage.js
+++ client/src/features/TableReservation/ReservationPage.js
@@ -1,14 +1,15 @@
 import { Component } from "react";
 import ReservationForm from "./ReservationForm";
+import { LoginContext } from "../SharedComponent/LoginContext";
 
 class ReservationPage extends Component {
   render() {
     return (
       <div class="col">
         <div class="text-center mt-5">
-          <h3>Nhà hàng Aprycot mở cửa từ 8:00 đến 22:00 hàng ngày</h3>
-          <h4>Vui lòng đặt bàn trước giờ dùng bữa 3 giờ</h4>
-          <h4><span class="text-decoration-none" style={{color: '#EA6A12'}}>Đăng nhập</span> để nhận ưu đãi phí đặt cọc</h4>
+          <h3>{this.context.lang === "vi" ? "Nhà hàng Aprycot mở cửa từ 8:00 đến 22:00 hàng ngày" : "Aprycot Restaurant opens daily from 8:00 AM to 10:00 PM"}</h3>
+          <h4>{this.context.lang === "vi" ? "Vui lòng đặt bàn trước giờ dùng bữa 3 giờ": "Please take the reservation before at least 3 hours."}</h4>
+          <h4><span class="text-decoration-none" style={{color: '#EA6A12'}}>{this.context.lang === "vi"? "Đăng nhập": "Sign In"}</span> {this.context.lang === "vi"? "để nhận ưu đãi phí đặt cọc!": "to receive deposit fee discount!"}</h4>
         </div>
         <div class="row g-3 flexbox">
           <div class="col-sm-6 d-flex justify-content-center">
@@ -27,4 +28,5 @@ class ReservationPage extends Component {
   }
 }
 
+ReservationPage.contextType = LoginContext;
 export default ReservationPage;
