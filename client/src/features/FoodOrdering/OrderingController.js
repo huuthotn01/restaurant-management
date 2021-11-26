6e00ed05ab859ea3318d0f724b56b72c1d497172
Gia Cat <giacatnk@gmail.com>
2021-11-26 23:49:03 +0700

    Update OrderingController.js

--- client/src/features/FoodOrdering/OrderingController.js
+++ client/src/features/FoodOrdering/OrderingController.js
@@ -4,6 +4,8 @@ import NavigationBar from './components/Menu/MenuView/NavigationView';
 import Menu from './components/Menu/MenuView/MainPageView';
 import Cart from './components/Cart/CartView/Cart';
 import CartProvider from './components/Cart/CartController/CartProvider';
+import { LoginContext } from '../SharedComponent/LoginContext';
+import { Redirect, Switch } from 'react-router';
 
 function Food_Ordering() {
   const [cartIsShown, setCartIsShown] = useState(false);
@@ -16,14 +18,21 @@ function Food_Ordering() {
     setCartIsShown(false);
   };
   
+  
   return (
-    <CartProvider>
-      {cartIsShown && <Cart onClose={hideCartHandler} />}
-      <NavigationBar onShowCart={showCartHandler} />
-      <main>
-        <Menu />
-      </main>
-    </CartProvider>
+    <LoginContext.Consumer>
+    {data => ( 
+        data.role !== "2" ? <Switch><Redirect to="/" /> </Switch> :
+        <CartProvider>
+        {cartIsShown && <Cart onClose={hideCartHandler} />}
+        <NavigationBar onShowCart={showCartHandler} />
+        <main>
+          <Menu />
+        </main>
+      </CartProvider>
+    )}
+    
+    </LoginContext.Consumer>
   );
 }
 
